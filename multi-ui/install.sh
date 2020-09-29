f32d9219e7eaa58006b816f69e61ccbe501644c1
jedc <jedcalkin@gmail.com>
2020-09-30 12:14:31 +1300

    Update install.sh

--- multi-ui/install.sh
+++ multi-ui/install.sh
@@ -1,7 +1,7 @@
-curl https://jedcalkin.github.io/deno-ts/multi-ui/config.json
-curl https://jedcalkin.github.io/deno-ts/multi-ui/ui-start.sh
+curl https://jedcalkin.github.io/deno-ts/multi-ui/config.json > config.json
+curl https://jedcalkin.github.io/deno-ts/multi-ui/ui-start.sh > ui-start.sh
 chmod +x ui-start.sh
 mkdir www
-curl https://jedcalkin.github.io/deno-ts/multi-ui/www/index.html
-curl https://jedcalkin.github.io/deno-ts/multi-ui/www/control-1.html
-curl https://jedcalkin.github.io/deno-ts/multi-ui/www/display-1.html
+curl https://jedcalkin.github.io/deno-ts/multi-ui/www/index.html > www/index.html
+curl https://jedcalkin.github.io/deno-ts/multi-ui/www/control-1.html > www/control-1.html
+curl https://jedcalkin.github.io/deno-ts/multi-ui/www/display-1.html > www/display-1.html
