efc05595677fb83134b007152fcb3c1e353c1b52
jedc <jedc@jedc.nz>
2020-09-30 19:07:25 +1300

    args

---
+++ args.ts
@@ -0,0 +1,44 @@
+import { get } from 'https://jedcalkin.github.io/deno-ts/get.ts'
+
+interface Args {
+  [arg: string]: any
+}
+
+ export default async function(config: Args): Promise<Args> {
+  
+  cli_args: {
+    var cmdArgs: Args = {}
+    let key: string = ''
+    let value: boolean = false
+    for(const arg of Deno.args) {
+      if(value){
+        cmdArgs[key] = value
+        value = false
+      }
+      if(arg.startsWith('--')){
+        let [k, v] = arg.split('=')
+        key = k.slice(2, Infinity)
+        if(v){
+          cmdArgs[key] = v
+        } else {
+          value = true
+        }
+      }
+    }
+  }
+
+  remote_args: {
+    var configStr = '{}'
+    if(cmdArgs.config){
+      try {
+        configStr = await get(cmdArgs.config)
+      } catch(e) {}
+    }
+  }
+
+  return {
+    ...JSON.parse(configStr),
+    ...config,
+    ...cmdArgs
+  }
+}
